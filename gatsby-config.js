69862a89abc7914e225bea8ccc0f0ed3b92060f5
kennedyc <charles.kennedy@computershare.co.uk>
2021-08-25 11:31:53 +0100

    Use CMS data in index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,13 @@ module.exports = {
           name: 'blog',
           path: 'src/blog'
         }
+	 },
+	 {
+		resolve: 'gatsby-source-filesystem',
+		options: {
+		  name: 'pageData',
+          path: 'src/pageData'
+		}
      },
 	'gatsby-transformer-remark'
   ]
